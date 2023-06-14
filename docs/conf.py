6b8362a1d9be16f98e630109f94e0ea4816bcc54
OliverSchw <95912395+OliverSchw@users.noreply.github.com>
2023-06-14 11:37:30 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
