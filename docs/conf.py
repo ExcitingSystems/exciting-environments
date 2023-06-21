b02f8e1b1f1223ee53956c50e4c91da165e0c837
OliverSchw <95912395+OliverSchw@users.noreply.github.com>
2023-06-21 11:54:13 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.setrecursionlimit(1500)
 
 project = 'Exciting Environments'
 copyright = '2023, Oliver Schweins'
@@ -31,6 +32,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+pygments_style = 'sphinx'
 
 master_doc = 'index'
 
