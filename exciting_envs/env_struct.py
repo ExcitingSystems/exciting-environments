9be59b1541fc9d17aac3f623f8de11a72dcba40d
Oliver Schweins <oliver_schw@gmx.de>
2023-06-22 12:06:58 +0200

    update docu

--- exciting_envs/env_struct.py
+++ exciting_envs/env_struct.py
@@ -101,10 +101,14 @@ class CoreEnvironment:
             action: Action to play on the environment.
 
         Returns:
-            observation(ndarray(float)): Observation/State Matrix: (shape=(batch_size,states)).
-            reward(ndarray(float)): Amount of reward received for the last step: (shape=(batch_size,1)).
-            terminated(bool): Flag, indicating if Agent has reached the terminal state.
-            truncated(ndarray(bool)): Flag, indicating if state has gone out of bounds: (shape=(batch_size,states)).
+            observation(ndarray(float)):
+                Observation/State Matrix: (shape=(batch_size,states)).
+            reward(ndarray(float)):
+                Amount of reward received for the last step: (shape=(batch_size,1)).
+            terminated(bool): 
+                Flag, indicating if Agent has reached the terminal state.
+            truncated(ndarray(bool)): 
+                Flag, indicating if state has gone out of bounds: (shape=(batch_size,states)).
             {}: An empty dictionary for consistency with the OpenAi Gym interface.
         """
         return
