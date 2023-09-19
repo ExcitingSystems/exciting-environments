47af867efd18f0c5d1ebe8691e8e3a8076c10eb6
Oliver Schweins <oliver_schw@gmx.de>
2023-09-19 14:13:07 +0200

    docu change

--- exciting_environments/registration.py
+++ exciting_environments/registration.py
@@ -3,14 +3,17 @@ from mass_spring_damper import MassSpringDamper
 from pendulum import Pendulum
 
 def make(env_id: str, **env_kwargs):
-    match env_id:
-        case 'CartPole-v0':
-            env=CartPole(**env_kwargs)
-        
-        case 'MassSpringDamper-v0':
-            env=MassSpringDamper(**env_kwargs)
+    if env_id== 'CartPole-v0':
+        env=CartPole(**env_kwargs)
+    
+    elif env_id=='MassSpringDamper-v0':
+        env=MassSpringDamper(**env_kwargs)
 
-        case 'Pendulum-v0':
-            env=Pendulum(**env_kwargs)
+    elif env_id=='Pendulum-v0':
+        env=Pendulum(**env_kwargs)
+
+    else:
+        print(f"No existing environments got env_id ={env_id}")
+        env=None
 
     return env
