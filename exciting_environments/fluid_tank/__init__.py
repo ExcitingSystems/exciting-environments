589cd06acb00faf331e084e1d62901f3d7f545a4
Hendrik Vater <vater@lea.uni-paderborn.de>
2024-07-10 15:30:33 +0200

    added fluid tank implementation as a new environment

---
+++ exciting_environments/fluid_tank/__init__.py
@@ -0,0 +1,1 @@
+from .fluid_tank_env import FluidTank
--- exciting_environments/registration.py
+++ exciting_environments/registration.py
@@ -1,6 +1,7 @@
 from .cart_pole import CartPole
 from .mass_spring_damper import MassSpringDamper
 from .pendulum import Pendulum
+from .fluid_tank import FluidTank
 
 
 def make(env_id: str, **env_kwargs):
@@ -12,6 +13,9 @@ def make(env_id: str, **env_kwargs):
 
     elif env_id == "Pendulum-v0":
         env = Pendulum(**env_kwargs)
+
+    elif env_id == "FluidTank-v0":
+        env = FluidTank(**env_kwargs)
 
     else:
         print(f"No existing environments got env_id ={env_id}")
