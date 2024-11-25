c12ef87e053ae68d9a4dc4cbaeaebc6f43a888d2
Oliver Schweins <oliver_schw@gmx.de>
2024-11-25 16:32:25 +0100

    added small docu change

--- exciting_environments/pmsm/pmsm_env.py
+++ exciting_environments/pmsm/pmsm_env.py
@@ -376,7 +376,7 @@ class PMSM(CoreEnvironment):
                 u_d_buffer=0.0,
                 u_q_buffer=0.0,
                 epsilon=0.0,
-                i_d=-env_properties.physical_constraints.i_d / 2,  # 0.0,
+                i_d=-env_properties.physical_constraints.i_d / 2,
                 i_q=0.0,
                 torque=0.0,
                 omega_el=(env_properties.physical_constraints.omega_el) / 2,
@@ -819,8 +819,7 @@ class PMSM(CoreEnvironment):
         sin_eps = jnp.sin(eps)
         obs = jnp.hstack(
             (
-                (system_state.physical_state.i_d + (physical_constraints.i_d * 0.5))
-                / (physical_constraints.i_d * 0.5),  #   system_state.physical_state.i_d/ physical_constraints.i_d,
+                (system_state.physical_state.i_d + (physical_constraints.i_d * 0.5)) / (physical_constraints.i_d * 0.5),
                 system_state.physical_state.i_q / physical_constraints.i_q,
                 system_state.physical_state.omega_el / physical_constraints.omega_el,
                 system_state.physical_state.torque / physical_constraints.torque,
