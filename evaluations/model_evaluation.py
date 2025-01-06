b4097b9f245c80d773a1abb0e6385ca04e913af5
Oliver Schweins <oliver_schw@gmx.de>
2025-01-06 13:20:38 +0100

    added model evaluator changes

--- evaluations/model_evaluation.py
+++ evaluations/model_evaluation.py
@@ -15,8 +15,8 @@ class PredictionComparison(eqx.Module):
     obs_dim: float
 
     def __init__(self, grid, action_dim, obs_dim):
-        assert (
-            grid.shape[-1] == (action_dim + obs_dim).astype(int)
+        assert grid.shape[-1] == (action_dim + obs_dim).astype(
+            int
         ), f"The grid dimension does not fit the action_dim and obs_dim. Grid dimension should be action_dim+obs_dim, but {grid.shape[-1]} and {action_dim}+{obs_dim} are given"
         self.grid = grid
         self.action_dim = action_dim
@@ -24,24 +24,23 @@ class PredictionComparison(eqx.Module):
 
     def __call__(self, model, model_gt, tau):
         pred = jax.vmap(model.step, in_axes=(0, 0, None))(
-            self.grid[:, :self.obs_dim], self.grid[:, self.obs_dim:], tau)
+            self.grid[:, : self.obs_dim], self.grid[:, self.obs_dim :], tau
+        )
         pred_gt = jax.vmap(model_gt.step, in_axes=(0, 0, None))(
-            self.grid[:, :self.obs_dim], self.grid[:, self.obs_dim:], tau)
+            self.grid[:, : self.obs_dim], self.grid[:, self.obs_dim :], tau
+        )
 
-        return (pred-pred_gt), jnp.mean((pred-pred_gt)**2)
+        return (pred - pred_gt), jnp.mean((pred - pred_gt) ** 2)
 
 
-class Evaluator(eqx.Module):
-
-    constraint_function: Callable
-    constraint_data_space_grid: jax.Array
-    gt_model: eqx.Module
-
-    def __init__(
-            self, constraint_function, gt_model, obs_dim, act_dim, validation_points_per_dim):
+class Evaluator:
+    def __init__(self, constraint_function, gt_model, obs_dim, act_dim, validation_points_per_dim):
         self.constraint_function = constraint_function
+        self.gt_model = gt_model
         self.constraint_data_space_grid = valid_space_grid(
-            constraint_function, obs_dim+act_dim, points_per_dim, -1, 1)
-
-        # create metrics with default params
-        # TODO tests?
+            constraint_function, obs_dim + act_dim, validation_points_per_dim, -1, 1
+        )
+        # create default metrics with default params
+        self.default_metrics = {
+            "jsd": PredictionComparison(grid=self.constraint_data_space_grid, action_dim=act_dim, obs_dim=obs_dim),
+        }
